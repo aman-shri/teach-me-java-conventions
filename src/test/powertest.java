c6a0c9a731da2f02b73bfc1dd007db3977040127
aman1108 <shrivastava250@gmail.com>
2024-04-19 11:07:25 +0530

    changed test methods names to standard camelCase

--- src/test/powertest.java
+++ src/test/powertest.java
@@ -5,22 +5,22 @@ import org.junit.Test;
 
 public class powertest {
     @Test
-    public void one_raised_to_one_is_onew() {
+    public void testOfOneRaisedToPowerOne() {
         assert powerfinder.OF(1,1) == 1;
     }
 
     @Test
-    public void twoRaised_to_one_is_2() {
+    public void testOfTwoRaisedToPowerOne() {
         assert powerfinder.OF(2,1) == 2;
     }
 
     @Test
-    public void two_power_2_is_4() {
+    public void testOfTwoRaisedToPowerTwo) {
         assert powerfinder.OF(2, 2) == 4;
     }
 
     @Test
-    public void power_of_2_and_3_Is_SIX() {
-        assert powerfinder.OF(3, 2) == 3*3;
+    public void testOfThreeRaisedToPowerTwo() {
+        assert powerfinder.OF(3, 2) == 9;
     }
 }
